0919f8578d39aa518fc9a0ff67a1c84b9e3bf276
Trey Woodward <trey@primitivesocial.com>
2025-05-30 23:54:21 -0500

    metadata changes

--- metadata-utils.js
+++ metadata-utils.js
@@ -1,34 +1,30 @@
-const piexif = require('piexifjs');
+const piexif = require("piexifjs");
 
-/**
- * Embeds metadata (description, folder, etc.) into a base64 image
- * @param {string} base64Image - The image in data URL format (e.g., "data:image/jpeg;base64,...")
- * @param {object} metadata - An object containing metadata fields like description, folder, etc.
- * @returns {string} - The base64 image string with embedded metadata
- */
 function embedMetadataToBase64(base64Image, metadata) {
-  // Strip off data URL prefix to get base64-encoded binary
-  const base64Data = base64Image.split(',')[1];
-  const binaryStr = Buffer.from(base64Data, 'base64').toString('binary');
+  const zeroth = {};
+  const exif = {};
 
-  // Create EXIF metadata
-  const exifObj = {
-    "0th": {
-      [piexif.ImageIFD.ImageDescription]: metadata.description || "",
-    },
-    "Exif": {
-      [piexif.ExifIFD.UserComment]: piexif.undefinedUnicode(metadata.description || ""),
-    },
-    "1st": {},
-    "thumbnail": null,
-  };
+  if (metadata.description) {
+    zeroth[piexif.ImageIFD.ImageDescription] = metadata.description;
+  }
 
+  if (metadata.folder) {
+    zeroth[piexif.ImageIFD.DocumentName] = metadata.folder;
+  }
+
+  if (metadata.projectId) {
+    zeroth[piexif.ImageIFD.PageName] = metadata.projectId;
+  }
+
+  if (metadata.createdDate) {
+    zeroth[piexif.ImageIFD.DateTime] = new Date(metadata.createdDate).toISOString();
+  }
+
+  const exifObj = { "0th": zeroth, Exif: exif };
   const exifBytes = piexif.dump(exifObj);
-  const newDataUrl = piexif.insert(exifBytes, base64Image);
+  const newData = piexif.insert(exifBytes, base64Image);
 
-  return newDataUrl;
+  return newData;
 }
 
-module.exports = {
-  embedMetadataToBase64,
-};
+module.exports = { embedMetadataToBase64 };
