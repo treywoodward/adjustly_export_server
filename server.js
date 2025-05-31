3fb7d4d2c6adb0093e3c6f12d7fec2a8b3dc27a1
Trey Woodward <trey@primitivesocial.com>
2025-05-31 15:18:04 -0500

    PORT updated from 3000

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.post('/export', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Export service running on port ${PORT}`);
 });
